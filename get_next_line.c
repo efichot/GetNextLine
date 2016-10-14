dd04e370c40088fc3ad0bb947cdba58e1a1caa02
Etienne Fichot <fichotetienne@gmail.com>
2016-10-14 18:58:55 +0200

    v0.1

--- get_next_line.c
+++ get_next_line.c
@@ -1,56 +1,106 @@
 #include "get_next_line.h"
 
-int ft_check_line(int ret, char **line, char **stock, char **tmp)
+int ft_open_failed(int ret, char **stock)
 {
-  *stock = NULL;
-  if (res == 0 && ft_strlen(tmp) > 0)
-  {
-
-  }
-  else
-    return (ret);
+  ft_putendl("Close() failed");
+  ft_strdel(stock);
+  return (ret);
 }
 
-char *ft_read_line(char *s)
+int		ft_eof(char **line, char **stock)
 {
-  int i;
-  char *line;
-
-  i = 0;
-  while (s[i] != '\n')
-    i++;
-  line = (char *)malloc(sizeof(*line) * i + 1);
-  if (!line)
-    return (NULL);
-  line = ft_strncpy(line, s, i);
-  line[i] = '\0';
-  return (line);
-}
-
-int get_next_line(int const fd, char **line)
-{
-  static char *stock = NULL;
-  char *buffer;
-  int ret;
-  char *tmp;
-
-  if (fd < 0 || !line || BUFF_SIZE < 0);
-    return (-1);
-  if (!stock)
-    stock = ft_memalloc(BUFF_SIZE);
-  tmp = ft_strcpy(ft_memalloc(BUFF_SIZE), stock);
-  buffer = ft_strnew(BUFF_SIZE);
-  while (!ft_strchr(tmp, '\n'))
-  {
-    if ((ret = read(fd, buffer, BUFF_SIZE) < 1)
-      return (ft_check_line(ret, line, &stock, &tmp));
-    buffer[ret] = '\0';
-    tmp = ft_strjoin(tmp, buffer);
-  }
-  *line = ft_read_line(tmp);
-  if (ft_strchr(tmp, '\n')
-    stock = ft_strcpy(stock, ft_strchr(tmp, '\n') + 1);
-  free(tmp);
-  free(buf);
+	if (ft_strlen(*stock) > 0)
+	{
+		*line = *stock;
+		ft_strdel(stock);
+	}
   return (1);
 }
+
+int		get_next_line(int const fd, char **line)
+{
+	static char		*stock = NULL;
+	int				ret;
+	char			*buffer;
+	char			*tmp;
+
+	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 10000000)
+		return (-1);
+	buffer = ft_strnew(BUFF_SIZE);
+	stock = (stock == NULL) ? ft_memalloc(BUFF_SIZE) : stock;
+	while (!(ft_strchr(stock, '\n')))
+	{
+		if (!(ret = read(fd, buffer, BUFF_SIZE)))
+			return (ft_eof(line, &stock));
+    if (ret == -1)
+      return (ft_open_failed(ret, &stock));
+		buffer[ret] = '\0';
+		stock = ft_strjoin(stock, buffer);
+	}
+	*line = ft_strsub(stock, 0, ft_strchrpos(stock, '\n'));
+	if (ft_strchr(stock, '\n'))
+  {
+    tmp = ft_strdup(stock);
+    ft_strdel(&stock);
+    stock = ft_strsub(tmp, ft_strchrpos(tmp, '\n') + 1, ft_strlen(tmp));
+    free(tmp);
+  }
+	free(buffer);
+	return (1);
+}
+//
+// int gnl_check_stock(char **stock, char **line)
+// {
+//   char *tmp;
+//
+//   if ((tmp = ft_strchr(*stock, '\n')))
+//   {
+//     *line = ft_strsub(*stock, 0, ft_strchrpos(*stock, '\n'));
+//     ft_strdel(stock);
+//     *stock = ft_strdup(tmp + 1);
+//     tmp = NULL;
+//     return (1);
+//   }
+//   return (0);
+// }
+//
+// int gnl_check_read(char *buffer, char **stock, char **line)
+// {
+//   char *tmp;
+//
+//   if ((tmp = ft_strchr(buffer, '\n')))
+//   {
+//     *line = ft_strjoin(*stock, ft_strsub(buffer, 0, ft_strchrpos(buffer, '\n')));
+//     ft_strdel(stock);
+//     *stock = ft_strdup(tmp);
+//     tmp = NULL;
+//     free(buffer);
+//     return (1);
+//   }
+//   else
+//     return (0);
+// }
+//
+// int get_next_line(int const fd, char **line)
+// {
+//   static char *stock = NULL;
+//   char *buffer;
+//   int ret;
+//
+//   ft_strdel(line);
+//   stock = (stock == NULL) ? ft_strnew(BUFF_SIZE) : stock;
+//   if (gnl_check_stock(&stock, line))
+//     return (1);
+//   buffer = ft_strnew(BUFF_SIZE);
+//   while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
+//   {
+//     buffer[ret] = '\0';
+//     if (gnl_check_read(buffer, &stock, line))
+//       return (1);
+//     stock = ft_strjoin(stock, buffer);
+//   }
+//   if (ret == -1)
+//     return (-1);
+//   if (!ft_strlen(stock))
+//     return (0);
+// }
