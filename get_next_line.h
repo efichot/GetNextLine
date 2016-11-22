cc031f90e5dba5c045d71c2b818a831183a56bc4
Etienne Fichot <fichotetienne@gmail.com>
2016-11-22 16:10:11 +0100

    Test

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efichot <efichot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/12 12:06:07 by efichot           #+#    #+#             */
+/*   Updated: 2016/11/14 20:55:53 by efichot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include "libft/includes/libft.h"
+# include "./libft/includes/libft.h"
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 1300
 
-int get_next_line(int const fd, char **line);
+int		get_next_line(int const fd, char **line);
 
 #endif
--- main.c
+++ main.c
@@ -1,20 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efichot <efichot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/12 13:35:35 by efichot           #+#    #+#             */
+/*   Updated: 2016/11/14 17:06:52 by efichot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int main(int argc, char **argv)
+int		main(int ac, char **av)
 {
-  int fd;
-  char *line;
-  int ret;
+	int		fd;
+	char	*line;
 
-  (void)argc;
-  fd = open(argv[1], O_RDONLY);
-  line = NULL;
-  while ((ret = get_next_line(fd, &line)))
-  {
-    ft_putendl(line);
-    free(line);
-    line = NULL;
-  }
-  close(fd);
-  return (0);
+	(void)ac;
+	if ((fd = open(av[1], O_RDONLY)) <= 0)
+	{
+		ft_putstr("Open() failed.\n");
+		return (-1);
+	}
+	line = NULL;
+	while (get_next_line(fd, &line))
+	{
+		ft_putstr("line:");
+		ft_putendl(line);
+		ft_strdel(&line);
+	}
+	close(fd);
+	return (0);
 }
