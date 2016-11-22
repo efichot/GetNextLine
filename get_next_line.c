d45839b944abf9c9b265115b4cc33b5823691720
Etienne Fichot <fichotetienne@gmail.com>
2016-11-22 16:09:21 +0100

    get_next_line.c
    Without bonus.

--- get_next_line.c
+++ get_next_line.c
@@ -1,50 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efichot <efichot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/12 12:06:57 by efichot           #+#    #+#             */
+/*   Updated: 2016/11/14 20:58:25 by efichot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int ft_read_failed(int ret, char **stock)
-{
-  ft_putendl_fd("Read() failed", 2);
-  ft_strdel(stock);
-  return (ret);
-}
-
-int		ft_eof(char **line, char **stock)
+int				ft_eof(char **line, char **stock, char **buffer)
 {
 	if (ft_strlen(*stock) > 0)
 	{
-		*line = *stock;
+		*line = ft_strdup(*stock);
 		ft_strdel(stock);
+		ft_strdel(buffer);
+		return (1);
 	}
-  return (0);
+	return (0);
 }
 
-int		get_next_line(int const fd, char **line)
+int				ft_failed(char **buffer, char **stock)
 {
-	static char		*stock = NULL;
-	int				ret;
+	ft_strdel(buffer);
+	ft_strdel(stock);
+	return (-1);
+}
+
+unsigned int	ft_strchrpos(char *stock, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (stock[i])
+	{
+		if (stock[i] == c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char			*ft_realloc_stk(char *tmp, char **stk)
+{
+	ft_strdel(stk);
+	*stk = ft_strdup(tmp);
+	ft_strdel(&tmp);
+	return (*stk);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static char		*stk = NULL;
 	char			*buffer;
+	int				ret;
 	char			*tmp;
 
-	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 10000000)
+	if (fd < 0 || !line || BUFF_SIZE < 1 || BUFF_SIZE > 1000000)
 		return (-1);
-	buffer = ft_strnew(BUFF_SIZE);
-	stock = (!stock) ? ft_memalloc(BUFF_SIZE) : stock;
-	while (!(ft_strchr(stock, '\n')))
+	stk = (!stk) ? ft_strnew(BUFF_SIZE) : stk;
+	while (!(ft_strchr(stk, '\n')))
 	{
+		buffer = ft_strnew(BUFF_SIZE);
 		if (!(ret = read(fd, buffer, BUFF_SIZE)))
-			return (ft_eof(line, &stock));
-    if (ret == -1)
-      return (ft_read_failed(ret, &stock));
-    buffer[ret] = '\0';
-		stock = ft_strjoin(stock, buffer);
+			return (ft_eof(line, &stk, &buffer));
+		if (ret == -1)
+			return (ft_failed(&buffer, &stk));
+		tmp = ft_strjoin(stk, buffer);
+		stk = ft_realloc_stk(tmp, &stk);
+		ft_strdel(&buffer);
 	}
-	*line = ft_strsub(stock, 0, ft_strchrpos(stock, '\n'));
-	if (ft_strchr(stock, '\n'))
-  {
-    tmp = ft_strdup(stock);
-    ft_strdel(&stock);
-    stock = ft_strsub(tmp, ft_strchrpos(tmp, '\n') + 1, ft_strlen(tmp));
-    free(tmp);
-  }
-	free(buffer);
+	*line = ft_strsub(stk, 0, ft_strchrpos(stk, '\n'));
+	if ((tmp = ft_strsub(stk, ft_strchrpos(stk, '\n') + 1, ft_strlen(stk))))
+		stk = ft_realloc_stk(tmp, &stk);
 	return (1);
 }
